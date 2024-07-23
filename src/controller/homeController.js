b3ec25e65700ee2ae83ac29e49df0f3147c54b80
Dat Phan <phandat0411@gmail.com>
2024-07-23 11:32:15 +0700

    create new user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,14 @@
+import mysql from 'mysql2';
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt',
+});
+
+
+
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs");
 }
@@ -6,6 +17,24 @@ const handleUserPage = (req, res) => {
     return res.render("user.ejs");
 }
 
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
+
+
+    connection.query(
+        ' INSERT INTO users (email, username, password) VALUES (?, ?, ?)', [email, username, password],
+        function (err, result, fields) {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result);
+        }
+    );
+    return res.send("handleCreateNewUser");
+}
+
 module.exports = {
-    handleHelloWorld, handleUserPage
+    handleHelloWorld, handleUserPage, handleCreateNewUser
 }
--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 import initWebRoutes from './routes/web';
+import bodyParser from 'body-parser';
 
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 //config view engine
 configViewEngine(app);
-
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 //init web routes
 initWebRoutes(app);
-
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);  // log server running message in console.log
 });
