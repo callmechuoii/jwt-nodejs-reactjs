b420b6341f58507de43f90b4f8f1aa0962450452
Dat Phan <phandat0411@gmail.com>
2024-07-25 16:53:09 +0700

    update user

--- src/service/userservice.js
+++ src/service/userservice.js
@@ -69,6 +69,40 @@ const deleteUser = async (id) => {
     }
 }
 
+const getUserById = async (id) => {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'jwt',
+        Promise: bluebird,
+    });
+    try {
+        const [rows, fields] = await connection.execute(
+            'SELECT * FROM users WHERE id=?', [id]
+        );
+        return rows;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const updateUserInfor = async (email, username, id) => {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'jwt',
+        Promise: bluebird,
+    });
+    try {
+        const [rows, fields] = await connection.execute(
+            'UPDATE users SET email = ?, username = ? WHERE id = ?', [email, username, id]
+        );
+        return rows;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 module.exports = {
-    createNewUser, getUserList, deleteUser
+    createNewUser, getUserList, deleteUser, getUserById, updateUserInfor
 }
